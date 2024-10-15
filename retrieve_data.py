44616e5977c5a99068d6c0cdebb2a93628e8f9f0
SniperDolphin45 <60530499+SniperDolphin45@users.noreply.github.com>
2024-10-14 21:02:59 -0400

    initial data and preprocessing components

---
+++ retrieve_data.py
@@ -0,0 +1,22 @@
+import requests
+import json
+def main():
+    date = "2020-06-01"
+    subreddit = "Republican"
+    search_term = "president"
+    posts_comments = "posts"
+    body_or_query = "body" if posts_comments == "comments" else "query"
+    url = f"https://arctic-shift.photon-reddit.com/api/{posts_comments}/search?sort=asc&after={date}&subreddit={subreddit}&{body_or_query}={search_term}&limit=100"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        responsedata = response.json()["data"]
+        with open(f'./data/{subreddit}_{posts_comments}.jsonl', "w") as f:
+            for comment in responsedata:
+                f.write(json.dumps(comment) + '\n')
+            f.close()
+    except requests.exceptions.RequestException as errex:
+        print(f"Invalid arguments for API query. Try again.")
+
+if __name__ == "__main__":
+    main()
