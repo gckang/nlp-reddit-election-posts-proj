3d494782e1196478cfeebc26200d40e41a81bfed
gckang <76260739+gckang@users.noreply.github.com>
2024-10-17 17:16:01 -0400

    added 2024 data

--- retrieve_data.py
+++ retrieve_data.py
@@ -1,17 +1,17 @@
 import requests
 import json
 def main():
-    date = "2020-06-01"
+    date = "2024-06-01"
     subreddit = "Republican"
     search_term = "president"
-    posts_comments = "posts"
+    posts_comments = "comments"
     body_or_query = "body" if posts_comments == "comments" else "query"
     url = f"https://arctic-shift.photon-reddit.com/api/{posts_comments}/search?sort=asc&after={date}&subreddit={subreddit}&{body_or_query}={search_term}&limit=100"
     try:
         response = requests.get(url)
         response.raise_for_status()
         responsedata = response.json()["data"]
-        with open(f'./data/{subreddit}_{posts_comments}.jsonl', "w") as f:
+        with open(f'./data/{subreddit}_{posts_comments}_2024.jsonl', "w") as f:
             for comment in responsedata:
                 f.write(json.dumps(comment) + '\n')
             f.close()
